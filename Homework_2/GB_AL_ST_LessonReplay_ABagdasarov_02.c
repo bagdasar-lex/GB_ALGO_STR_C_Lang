3980ced8d626f852fac5033753bf65486c1d0449
bagdasar-lex <bagdasar.lex@gmail.com>
2021-05-16 17:30:02 +0300

    Часть ветвящийся алгоритм

--- Homework_2/GB_AL_ST_LessonReplay_ABagdasarov_02.c
+++ Homework_2/GB_AL_ST_LessonReplay_ABagdasarov_02.c
@@ -1,15 +1,58 @@
-#include <stdio.h>
+/* #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
   float num1;
   float num2;
   float result;
 
-  printf("%s\n","Enter first number");
+  printf("%s\n","Enter first number:");
   scanf("%f",&num1);
-  printf("%s\n","Enter second number");
+  printf("%s\n","Enter second number:");
   scanf("%f",&num2);
   result = num1 + num2;
   printf("Result is: %f\n",result);
+  return 0;
+}
+*/
+
+
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+  float num1;
+  float num2;
+  float result;
+  char sign;
+
+  printf("%s","Enter first number: " );
+  scanf("%f",&num1 );
+
+  printf("%s","Enter operator: " );
+  fseek(stdin, 0, SEEK_END);
+  scanf("%c",&sign );
+
+  printf("%s","Enter second number: " );
+  scanf("%f",&num2 );
+  if (!(sign == '/' && num2 == 0 )) {
+  switch (sign) {
+    case '+':
+          result = num1 + num2;
+          break;
+    case '-':
+          result = num1 - num2;
+          break;
+    case '*':
+          result = num1 * num2;
+          break;
+    case '/':
+          result = num1 / num2;
+          break;
+    }
+      printf("Result is: %f\n",result );
+  } else {
+    printf("%s\n", "Incorect input, division by zero!" );
+  }
   return 0;
 }
